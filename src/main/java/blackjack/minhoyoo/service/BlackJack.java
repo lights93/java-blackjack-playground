458c56bd18c7206c7d5ed2ff449c8445080e72f8
lights93 <lights8615@gmail.com>
2022-03-30 01:26:18 +0900

    refactor: 서비스 리팩토링

--- src/main/java/blackjack/minhoyoo/service/BlackJack.java
+++ src/main/java/blackjack/minhoyoo/service/BlackJack.java
@@ -26,24 +26,46 @@ public class BlackJack {
 		Dealer dealer = new Dealer();
 		CardOwners cardOwners = new CardOwners(players, dealer);
 
+		initCards(cardOwners);
+
+		updateCards(players, dealer);
+
+		updateMoney(cardOwners);
+	}
+
+	private List<Player> getPlayers(Names names) {
+		return names.getValues().stream()
+			.map(name -> new Player(name, getMoney(name.getValue())))
+			.collect(Collectors.toList());
+	}
+
+	private Money getMoney(String name) {
+		try {
+			return Money.from(InputView.getMoney(name));
+		} catch (IllegalArgumentException e) {
+			ResultView.printMessage(e.getMessage());
+			return getMoney(name);
+		}
+	}
+
+	private Names getNames() {
+		try {
+			return Names.from(InputView.getNames());
+		} catch (IllegalArgumentException e) {
+			ResultView.printMessage(e.getMessage());
+			return getNames();
+		}
+	}
+
+	private void initCards(CardOwners cardOwners) {
 		cardOwners.drawCard(deck);
 		cardOwners.drawCard(deck);
-		printStatus(cardOwners);
-
-		players.forEach(this::setCards);
-		drawDealerCards(dealer);
-
-		cardOwners.updateMoney();
 		printStatus(cardOwners);
 	}
 
-	private void drawDealerCards(Dealer dealer) {
-		BlackjackResult blackjackResult = dealer.calculateResult();
-		if(!blackjackResult.isDealerEnd()) {
-			ResultView.printMessage("딜러는 16이하라 한장의 카드를 더 받았습니다.");
-			dealer.addCard(deck.draw());
-			drawDealerCards(dealer);
-		}
+	private void updateCards(List<Player> players, Dealer dealer) {
+		players.forEach(this::setCards);
+		drawDealerCards(dealer);
 	}
 
 	private void setCards(Player player) {
@@ -70,28 +92,18 @@ public class BlackJack {
 		}
 	}
 
-	private Names getNames() {
-		try {
-			return Names.from(InputView.getNames());
-		} catch (IllegalArgumentException e) {
-			ResultView.printMessage(e.getMessage());
-			return getNames();
+	private void drawDealerCards(Dealer dealer) {
+		BlackjackResult blackjackResult = dealer.calculateResult();
+		if(!blackjackResult.isDealerEnd()) {
+			ResultView.printMessage("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+			dealer.addCard(deck.draw());
+			drawDealerCards(dealer);
 		}
 	}
 
-	private Money getMoney(String name) {
-		try {
-			return Money.from(InputView.getMoney(name));
-		} catch (IllegalArgumentException e) {
-			ResultView.printMessage(e.getMessage());
-			return getMoney(name);
-		}
-	}
-
-	private List<Player> getPlayers(Names names) {
-		return names.getValues().stream()
-			.map(name -> new Player(name, getMoney(name.getValue())))
-			.collect(Collectors.toList());
+	private void updateMoney(CardOwners cardOwners) {
+		cardOwners.updateMoney();
+		printStatus(cardOwners);
 	}
 
 	private void printStatus(CardOwner cardOwner) {
